5b964fcfc649e9a8c4df9e2940d9d38db16ed49c
MgrGracz <kamil.bandzwolek@gmail.com>
2024-04-14 15:53:35 +0200

    v024
    Tabs

--- meetcast/resources/views/layouts/app.blade.php
+++ meetcast/resources/views/layouts/app.blade.php
@@ -43,88 +43,8 @@
                 </header>
 
                 {{-- Tab section --}}
-                <section
-                 x-data="{tab:'1'}"
-                 class="mb-auto overflow-y-auto overflow-x-scroll relative">
+                <livewire:components.tabs />
 
-                    <header class="flex items-center gap-5 mb-2 p-4 sticky top-0 bg-white z-10">
-
-                        <button @click="tab='1'" :class="{ 'border-b-2 border-violet-500': tab=='1' }" class="font-bold text-sm px-2 pb-1.5">
-                            Matches
-                            <span class="rounded-full tx-xs  p-1 px-2 font-bold text-white bg-meetcast">
-                                12
-                            </span>
-                        </button>
-
-                        <button @click="tab='2'" :class="{ 'border-b-2 border-violet-500': tab=='2' }" class="font-bold text-sm px-2 pb-1.5">
-                            Messages
-                            <span class="rounded-full tx-xs  p-1 px-2 font-bold text-white bg-meetcast">
-                                1
-                            </span>
-                        </button>
-
-                    </header>
-                    <main>
-                        {{-- matches --}}
-                        <aside class="px-2" x-show="tab=='1'">
-                            <div class="grid grid-cols-3 gap-2">
-
-                                @for ($i=0; $i< 3; $i++)
-                                    
-                                
-                                <div class="relative">
-                                    {{-- dot --}}
-                                    <span class="-top-6 -right-5 absolute">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dot text-violet-500 w-12 h-12" viewBox="0 0 16 16">
-                                            <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3"/>
-                                          </svg>
-                                    </span>
-                                    <img src="https://source.unsplash.com/200x200?face-woman-{{$i}}" alt="image" class="h-36 rounded-lg object-cover">
-                                   
-                                    {{-- name --}}
-                                    <h5 class="absolute rounded-lg bottom-2 bo left-2 text-white font-bold text-xs">
-                                        {{fake()->name}}
-                                    </h5>
-                                </div>
-                                @endfor
-                            </div>
-                        </aside>
-                        {{-- messages --}}
-                        <aside x-cloak class="px-2 " x-show="tab=='2'">
-                            <ul>
-                                @for ($i =0; $i < 2; $i++)
-                                <li>
-                                    <a
-                                     @class(['flex gap-4 items-center p-2','border-r-4 border-violet-500 bg-white py-3'=>$i==3?true:false])
-                                     href="#">
-                                     
-                                     <div class="relative">
-                                        <span class="inset-y-0 my-auto absolute -right-7">
-                                            <svg 
-                                                @class([
-                                                    'w-14 h-14 stroke-[0.3px] stroke-white',
-                                                    'hidden'=>$i==3?false:true,
-                                                    'text-violet-500' =>true
-                                                ])
-                                             xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dot" viewBox="0 0 16 16">
-                                                <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3"/>
-                                              </svg>
-                                        </span>
-                                        <x-avatar class="h-14 w-14" src="https://source.unsplash.com/200x200?face-woman-{{$i}}" />
-                                     </div>
-
-                                     <div class="overflow-hidden">
-                                        <h6 class="font-bold truncate">{{fake()->name}}</h6>
-                                        <p class="text-gray-600 truncate">{{fake()->text}}</p>
-                                     </div>
-                                    </a>
-                                </li>
-                                @endfor
-                            </ul>
-                        </aside>
-                    </main>
-
-                </section>
             </aside>
 
             <!-- Page Content -->
